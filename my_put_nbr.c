bd17293d292ee7ef71a409888af68692d5bbbf42
Otyne <43828915+otyne@users.noreply.github.com>
2018-10-04 17:13:35 +0200

    update the display of 0

--- my_put_nbr.c
+++ my_put_nbr.c
@@ -7,17 +7,28 @@
 
 #include "my.h"
 
-int my_put_nbr(int nb)
+static int display_nbr(int nb)
 {
-    char n;
-    
     if (nb < 0) {
 	my_putchar('-');
-	nb *= -1;
+	n = nb % 10 * -1 + '0';
+	display_nbr(nb / -10);
+	my_putchar(n);
     }
     if (nb > 0) {
 	n = nb % 10 + '0';
-	my_put_nbr(nb / 10);
+	display_nbr(nb / 10);
 	my_putchar(n);
     }
+}
+
+int my_put_nbr(int nb)
+{
+    char n;
+
+    if (nb == 0) {
+        my_putchar('0');
+        return (0);
+    }
+    display_nbr(nb);    
 }

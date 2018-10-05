b811e74edf0c87544e769f2418cc4260db7532e2
Tina <skielred@gmail.com>
2018-10-05 11:32:06 +0200

    Remove unused var, remove multiple check on number sign

--- my_put_nbr.c
+++ my_put_nbr.c
@@ -7,28 +7,24 @@
 
 #include "my.h"
 
-static int display_nbr(int nb)
+static void display_nbr(int nb)
 {
-    if (nb < 0) {
-	my_putchar('-');
-	n = nb % 10 * -1 + '0';
-	display_nbr(nb / -10);
-	my_putchar(n);
-    }
     if (nb > 0) {
-	n = nb % 10 + '0';
 	display_nbr(nb / 10);
-	my_putchar(n);
+	my_putchar(nb % 10 + '0';);
     }
 }
 
 int my_put_nbr(int nb)
 {
-    char n;
-
     if (nb == 0) {
         my_putchar('0');
         return (0);
     }
-    display_nbr(nb);    
+    if (nb < 0) {
+        nb = -nb;
+        my_putchar('-');
+    }
+    display_nbr(nb);
+    return (0);
 }
